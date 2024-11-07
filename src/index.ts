fde50a9695e77fa586edf485a772fb1872134d56
jamay byrone <jamie@jamiebyrne.com>
2024-11-07 22:51:27 +0000

    fix: build type definitions
    - updated tsconfig
    - added in index.ts's
    - updated types and files

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as RichBreadCrumbs } from './RichBreadCrumbs';
+export { default as RichDataset } from './RichDataset';
+export { default as RichEvent } from './RichEvent';
+export { default as RichFaqs } from './RichFaqs';
+export { default as RichOrganization } from './RichOrganization';
+export { default as RichPhotos } from './RichPhotos';
+export { default as RichProduct } from './RichProduct';
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export { BreadcrumbItem, RichBreadCrumbDataProps } from './breadcrumb';
+
+export { DataSetType, RichDatasetProps } from './dataset';
+
+export { RichEventType, RichEventProps } from './event';
+
+export { FaqAcceptedAnswer, FaqEntity, FaqProps } from './faqs';
+
+export { PhotoType, RichPhotoProps } from './photos';
+
+export { ProductType, RichProductProps } from './product';
+
+export { OrganizationType, RichOrganizationProps } from './organization';
